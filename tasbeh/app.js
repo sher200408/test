4ebe78c8841cdc00a6c42bf2816fe2b633a13164
sher200408 <113122559+sher200408@users.noreply.github.com>
2023-06-29 11:39:08 +0500

    Add files via upload

---
+++ tasbeh/app.js
@@ -0,0 +1,14 @@
+// for (let i = 100; i>10; i-- ){
+//     console.log(i)
+
+// }
+// let a,b = +prompt("son kidin")
+
+// for (i=1; i<b ;i++ ){
+//     console.log(a<b +" "+i)
+// }
+
+// for (let g = 30; g<200 ;g++){
+//     console.log(g)
+// }
+
